95be89f19b034f47ae2266e191fcd450bd45807d
LAPTOP-G2A3P8N8\franc <francesco.nespoli@hotmail.it.it>
2019-04-24 13:03:57 +0200

    Update Ekg_Olimex.py
    Succesful reading of the byte stream for serial port and decoding the header of the Olimex packet

--- Ekg_Olimex.py
+++ Ekg_Olimex.py
@@ -1,29 +1,32 @@
 import serial
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.signal
+import binascii
+import codecs
+# import sys
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import scipy.signal
 
+### PARAMETERS ###
 
-ser = serial.Serial('COM3') #, 38400, timeout=0,parity=serial.PARITY_EVEN, rtscts=1)
+N=100 # number of byte (every packet is 17 bytes)
 
-# 38400, n, 8, 1
+def Initiate_serial_comunication(port_ID,baudrate_in):
+    ser = serial.Serial(port_ID, baudrate_in,timeout=None)  # baudrate should be the same as the arduino serial port frequency and timeout none as specified by Olimex
+    return ser
 
-#indica una trasmissione con baud rate di 9600, senza parità (n = none - no parity), con dato a 8 bit e 1 bit di Stop.
-s = ser.read(100)
-ss = s.decode("utf-8")
-sss=ss.split("_")
+# print(s)
+# x=binascii.hexlify(s) #from bytes to hex
+# y=codecs.decode(x,'hex') #from hex to bytes
+# z=x.decode()
+# print(z)
 
-text_file = open("Outputfake.txt", "w")
-text_file.write("Purchase Amount: %s" % sss)
-text_file.close()
-
-
-# x=np.linspace(0,1,len(sss))/9600
-# b,a=scipy.signal.butter(5,0.9)
-# y=scipy.signal.lfilter(b,a,sss)
-# plt.plot(x,y)
-# plt.show()
-
-
+if '__name__==__main':
+    ser=Initiate_serial_comunication('COM3',57600)
+    s = ser.read(N)
+    i=0
+    while i<(len(s)-1):
+        if (hex(s[i])=='0xa5') & (hex(s[i+1])=='0x5a'):
+            print('ciao')
+        i=i+1
+        # temp_int = int(temp_hex, 16)
 
