4c5d8a7b05e9de2bb9aa8db7742a0f0710a86cb9
LAPTOP-G2A3P8N8\franc <francesco.nespoli@hotmail.it.it>
2019-04-24 20:17:08 +0200

    Update Arduino_Data_Elaboration.py
    Included just a single function for importing data

--- Arduino_Data_Elaboration.py
+++ Arduino_Data_Elaboration.py
@@ -2,19 +2,13 @@ from numpy import linspace
 import matplotlib.pyplot as plt
 
 def Import_Data(filename):
-    f = open(filename)
-    data = f.read()
-    data_parsed = data.split(',')
-    return data_parsed
-
-def Import_Data_Old(filename): #txt files salvati in data 04/24/2019 si importano con questa funzione
     f=open(filename)
     data=f.read()
     return data.split('\n')
 
 
 if "__name__==__main__":
-    data = Import_Data_Old('your_file.txt')
+    data = Import_Data('your_file.txt')
     x=linspace(0,1,len(data))
     print(data)
     plt.plot(x,data)
